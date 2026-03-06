d2dab5cb39cf25a20d338f454175a9b8aa00b74b
ryan.dias <ryan.dias@agi.com.br>
2026-03-06 17:05:48 -0300

    feat:Transanção Strategy Pattern

--- src/main/java/com/ryan/servicepayment/repository/ContaRepository.java
+++ src/main/java/com/ryan/servicepayment/repository/ContaRepository.java
@@ -1,7 +1,6 @@
 package com.ryan.servicepayment.repository;
 
 import com.ryan.servicepayment.model.Conta;
-import com.ryan.servicepayment.model.Transacao;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,7 +10,5 @@ import java.util.List;
 public interface ContaRepository extends MongoRepository<Conta, String> {
 
     List<Conta> findByCpf(String cpf);
-
-
 
 }
